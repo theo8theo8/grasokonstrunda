e7feb387ecbddda08aa94867a7c506051f1fafe7
Theo Meier Ström <meier.strom.theo@gmail.com>
2023-02-21 14:42:28 +0100

    wip

---
+++ components/allArtists.tsx
@@ -0,0 +1,123 @@
+import styles from '@/styles/Home.module.css'
+
+export default function AllArtists() {
+    return(
+        <div className={styles.konstnarer}>
+            <div className={styles.placeBox}>
+                <h2>1. Rävsten</h2>
+                <p><em>Här finns lunch och fika. Passbåt från äspskärs brygga måste förbokas digitalt på www.rävsten.se</em></p><br/>
+                <p><b>Louise Westerberg</b><br/><em>Fårskinn, almanackor, skärgårdsbilder</em></p><br/>
+                <p><b>Jörgen Carlsson</b><br/><em>Akvarell, olja</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>2. Idö gård</h2>
+                <p><b>AnnaKarin Boode</b><br/><em>Skulpturer, målningar</em></p><br/>
+                <p><b>Johanna Florenius</b><br/><em>Textil</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>3. Bjurön</h2>
+                <p><em>Fika med hembakat</em></p><br/>
+                <p><b>Eva-Karin Lundberg</b><br/><em>Oljemålningar, trädgård</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>4. Högklyke</h2>
+                <p><em>Fika, en fiol, några folkmusiklåtar under dagen</em></p><br/>
+                <p><b>Elisabeth Lundgren</b><br/><em>Fotografi - ögonblick med fåglar, landskap</em></p><br/>
+                <p><b>Britt-Marie Jansson</b><br/><em>Akvarell, måleri landskap och blommor</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>5. Muskaregrund</h2>
+                <p><em>Fika</em></p><br/>
+                <p><b>Berit Blom</b><br/><em>Handarbeten, stickat och broderat</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>6. Församlingshemmet</h2>
+                <p><em>Fika och musik som dyker upp när man minst anar det</em></p><br/>
+                <p><b>Åsa Gröndal</b><br/><em>Akrylmålningar</em></p><br/>
+                <p><b>Jessica Hörner</b><br/><em>Fotografier</em></p><br/>
+                <p><b>Helena Hörner</b><br/><em>Stickat</em></p><br/>
+                <p><b>Lilian Wickholm</b><br/><em>Akryl, olja, akvarell</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>7. Gräsö Camping Restaurang Doppingen</h2>
+                <p><em>Fika, mat, glass, bad m.m. Här finns handikapptoalett</em></p><br/>
+                <p><b>Mimmi Dubén</b><br/><em>Måleri akryl</em></p><br/>
+                <p><b>Gunilla Skoglund</b><br/><em>Oljemåleri</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>8. Nötsveden</h2>
+                <p><b>Sören Lindström</b><br/><em>Trähantverk svepteknik, svarvat, luffarslöjd</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>9. Oxsten</h2>
+                <p><b>Marina Laurent</b><br/><em>Fotografier "Natur i närbild"</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>10. Västerbyn</h2>
+                <p><em>Fika</em></p><br/>
+                <p><b>Gunilla Andersson</b><br/><em>Oljemåleri</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>11. Djursten</h2>
+                <p><b>Göte Andersson</b><br/><em>Träslöjd</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>12. Väster Mörtarö Studio 838</h2>
+                <p><b>Martin Lima de Faria</b><br/><em>Fotokonst med miniatyrer m.m</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>13. Anders Matsgården</h2>
+                <p><em>Café. Antikviteter, vintage, retro. Silverutställning - äldre silver från trakten.</em></p><br/>
+                <p><b>Grävlings Naturprodukter Gräsö</b><br/><em>Naturnära hantverk, ljus m.m tillverkat på Gräsö</em></p><br/>
+                <p><b>Staffan Strand</b><br/><em>Bronsskulpturer, gipser</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>14. Sjöäng</h2>
+                <p><b>Kerstin Sjöblom</b><br/><em>Akryl, olja, akvarell</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>15. Norrboda Bygdegård</h2>
+                <p><em>Café. Här finns handikapptoalett</em></p><br/>
+                <p><b>Lars Bäcklund</b><br/><em>Trä och läder</em></p><br/>
+                <p><b>Gun Asplind</b><br/><em>Olja, akvarell, litografi, posters, vykort</em></p><br/>
+                <p><b>Thomas Olofsson</b><br/><em>Knivar</em></p><br/>
+                <p><b>Monica Ohlsson</b><br/><em>Personliga fotografier från Gräsö</em></p><br/>
+                <p><b>Paulina Partanen</b><br/><em>Olja, akvarell, akryl</em></p><br/>
+                <p><b>Peter Backemar</b><br/><em>Ställer ut sin bok "Livet på en ö i Roslagen under första delen av 1900-talet"</em></p><br/>
+                <p><b>Lena Dalnert</b><br/><em>Akvarell</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>16. Västerhamnen Norrboda</h2>
+                <p><b>Vega Zetterström</b><br/><em>Akvarell, akryl</em></p><br/>
+                <p><b>Lars-Gunnar Bråvander</b><br/><em>Foton, akvarell, akryl m.m</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>17. Västerhamnen Norrboda</h2>
+                <p><b>Elin Lindqvist</b><br/><em>Festfint "mina ängder gröna" dukningskonst och blomsterinstallationer</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>18. Lökäng</h2>
+                <p><em>Musik måndag, onsdag, fredag och lördag 12.00</em></p><br/>
+                <p><b>Margit Martens</b><br/><em>Keramik</em></p><br/>
+                <p><b>Hans-åke Lerin</b><br/><em>Fotografier, fotobok</em></p><br/>
+                <p><b>Anna Karlsson</b><br/><em>Textilkonst-botaniska tryck på tyg och papper samt broderi</em></p><br/>
+                <p><b>Sanne Vils</b><br/><em>Waldorfdockor och silver</em></p><br/>
+                <p><b>Jon Gerbrand</b><br/><em>Lampor</em></p><br/>
+                <p><b>Dennis Persson</b><br/><em>Skulptur i återbruk, fotomontage, målningar</em></p><br/>
+                <p><b>Ulla Gerbrand</b><br/><em>Olja, akryl, grafik</em></p><br/>
+                <p><b>Kyll Hellberg</b><br/><em>Waldorfdockor och änglar</em></p><br/>
+                <p><b>Eleonore Holmgren Uppsalaförfattare</b><br/><em>På tisdag pratar hon om sin bok "Sista sommaren" som är nominerad till årets bok</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>19. Ängstorpet Norrboda</h2>
+                <p><b>Roland Kallström</b><br/><em>Konst och hantverk</em></p><br/>
+            </div>
+            <div className={styles.placeBox}>
+                <h2>20. Hamnen Källskärsudden</h2>
+                <p><em>Ca 200m att gå ifrån parkeringen. Fika finns</em></p><br/>
+                <p><b>Carina Pålgård</b><br/><em>Måleri akryl, abstrakt och intuitivt</em></p><br/>
+                <p><b>Irene Wåhlin</b><br/><em>Akryl, akvarell, blandteknik, foto, textil, hantverk i olika material</em></p><br/>
+            </div>
+        </div>
+    )
+}
