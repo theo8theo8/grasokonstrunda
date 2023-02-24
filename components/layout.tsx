86e92759d33cb89e845ffd8e5ae53d4f00f7563d
Theo Meier Ström <meier.strom.theo@gmail.com>
2023-02-24 17:27:20 +0100

    Moved font-import

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,9 +7,6 @@ export default function Layout({children, title="default title"}) {
       <>
         <Head>
             <title>{title}</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet"/>
             <meta name="description" content={title} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon-16x16.png" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Comfortaa} from '@next/font/google'
+
+const comfortaa = Comfortaa({weight: '400', subsets: ['latin-ext'], display: 'swap'})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${comfortaa.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
