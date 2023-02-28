341b7ac209ee194b40c345fdd2693aca47f1d45e
Theo Meier Ström <meier.strom.theo@gmail.com>
2023-02-28 23:01:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    images: {
-        unoptimized: true,
-    },
-    // assetPrefix: './',
-    trailingSlash: true,
     reactStrictMode: true,
+    darkmode: false
 }
