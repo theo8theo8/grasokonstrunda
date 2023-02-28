60233dc3aa931693647c0a9bc358acbf889fc341
Theo Meier Ström <meier.strom.theo@gmail.com>
2023-02-28 22:17:13 +0100

    Ny font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {Comfortaa} from '@next/font/google'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import localFont from '@next/font/local'
 config.autoAddCss = false
 
-const comfortaa = Comfortaa({weight: '400', subsets: ['latin-ext'], display: 'swap'})
+const font = localFont({ src: './Comfortaa-VariableFont_wght.woff2' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${comfortaa.style.fontFamily};
-        }
-      `}</style>
+    <main className={font.className}>
       <Component {...pageProps} />
-    </>
+    </main>
   )
 
 }
