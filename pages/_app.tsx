c5797417588a93e84a6de7bf0012b27de1ccf4c3
Theo Meier Ström <meier.strom.theo@gmail.com>
2023-05-10 16:32:36 +0200

    remove vercel insight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import localFont from '@next/font/local'
-config.autoAddCss = false
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import localFont from "@next/font/local";
+config.autoAddCss = false;
 
-const font = localFont({ src: './Comfortaa-VariableFont_wght.woff2' })
+const font = localFont({ src: "./Comfortaa-VariableFont_wght.woff2" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
       <Component {...pageProps} />
-      <Analytics/>
     </main>
-  )
+  );
 }
